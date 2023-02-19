8ab4edb196dc70c366bee235a39c1f931dc7ff0e
nhutb1910275 <nhutb1910275@student.ctu.edu.vn>
2023-02-19 12:23:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cros = require("cors");
 const app = express();
-// const ApiError = require("./app/api-error")
+const ApiError = require("./app/api-error")
 app.use(cros());
 app.use(express.json())
 app.get("/", (req, res) => {
@@ -12,14 +12,15 @@ const contactsRouter = require("./app/routes/contact.route.js");
 
 app.use("/api/contacts", contactsRouter);
 
-// //  cai dat cac xu li loi
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found !!!"));
-// });
-
-// app.use((err, req, res, next) => {
-
-//     return res.status(Error.statusCode || 500).json({ message: Error.message || "Internal Server Error", });
-// });
+app.use("/api/contacts", contactsRouter);
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(Error.statusCode || 500).json({
+        message: Error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
